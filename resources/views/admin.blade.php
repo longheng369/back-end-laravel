dd9b9eba3aba9a480552996f7c4a8963366b597a
longheng369 <longhengchhoeun@gmail.com>
2024-05-25 22:11:15 +0700

    second commit

--- resources/views/admin.blade.php
+++
@@ -1,1 +0,0 @@
-<div>welcome to admin</div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,7 @@ export default {
         './app/Filament/**/*.php',
         './resources/views/filament/**/*.blade.php',
         './vendor/filament/**/*.blade.php',
+        
     ],
 
     presets: [preset],
